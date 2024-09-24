83f22e326f007a3aade22ecb99910a8dcfad0e86
EC2 Default User <ec2-user@ip-172-31-42-46.ap-northeast-1.compute.internal>
2024-09-24 04:43:07 +0000

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "244268b52a89": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
